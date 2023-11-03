b005798af39a0d17f767de78520ec051ade308c5
karimdib <karim.dib2001@gmail.com>
2023-11-03 13:25:55 +0100

    prova vite

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script >
+export default{
+  data(){
+    return{
+      message:'ciao come stai?'
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <div class="messaggio">{{ message }}</div>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style >
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.messaggio{
+  color: greenyellow;
+}
+</style>
